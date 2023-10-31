c227f94a3e5a22137c00822da671ab3d1a252e7c
Seong <ls888806@naver.com>
2023-10-31 23:19:01 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { movies } from "./moviesDummy";
+import Movies from "./components/Movies";
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+function App() {
+    return(
+        <div>
+
+        </div>
+    );
+
+}
+
+export default App;
